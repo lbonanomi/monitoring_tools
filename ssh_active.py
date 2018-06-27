ac8657dab4bbed23fba0cb4fdbf30b6e34c83db8
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2018-06-27 08:40:43 -0400

    Update ssh_active.py
    TIL ".succeeded" and ".failed" are for commands, not for the paramiko connections

--- ssh_active.py
+++ ssh_active.py
@@ -1,39 +1,40 @@
 #!/bin/python
 
-"""Fabric to check SSH connectivity"""
-
-# Fabric task-runner to check SSH connectivity. 
-#
-# If a host fails SSH connect-checks 6 times in 30 minutes
-# fire the (placeholder) 'alert' function. WhisperDB will
-# stores value
+"""
+Fabric to check SSH connectivity
+"""
 
 from fabric.api import *
+#from fabric.contrib.console import confirm
+from fabric.exceptions import NetworkError
+
 
 import os
-import string
+
 import sys
+
 import whisper
+
+import string
 
 
 with open('host_list.tmp') as listing:
     env.hosts = map(string.strip, listing.readlines())
 
-    
+
 env.parallel = True     # like '-P' for parallel exec
-env.pool_size = 10      # like '-z' for pool-sizing
-env.timeout = 60        # SSH timeout in seconds
+env.pool_size = 2       # like '-z' for pool-sizing
+env.timeout = 6     # SSH timeout in seconds
 
 
 def alert(hostname):
-    """Alerting mechanism"""
     print "Launch alerts for " + hostname + "\n"
 
 
-def connect_check():
+def uptime():
     retainer = [(1800, 6)]      # [(seconds_in_period, slots_in_period)]
 
-    whisper_db_dir = '/var/tmp/whisperDB/'
+    whisper_db_dir = '/bb/data/tmp/whisperDB/'
 
     whisper_db_name = whisper_db_dir + env.host_string + '.wsp'
 
@@ -44,20 +45,21 @@ def connect_check():
         whisper.create(whisper_db_name, retainer)
 
     with hide('stdout', 'stderr', 'running'):
-       connector = run('uptime')
+        connector = run('uptime')
 
-    if connector.succeeded:
+    try:
+        connector = run('uptime')
+
         whisper.update(whisper_db_name, 0)
 
         (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
 
-    if connector.failed:
-        print "Gorked connecting-to: " + env.host_string
 
+    except NetworkError as e:
         whisper.update(whisper_db_name, 1)
 
         # Count failures
         (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
 
-        if fail_buffer.count(1) > 6:
-            alert(env.host_string)  
+        if fail_buffer.count(1) > 2:
+            alert(env.host_string)
