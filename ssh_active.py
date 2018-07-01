20f4522fccba1fe22749dfd1ebc6b8e72ac4d144
Luke Bonanomi <lbonanomi@gmail.com>
2018-06-30 22:06:07 -0400

    * Fixed stupid identation error * Sane monitor thresholds
    Experimental: If a host begins alarming, reduce alarm frequency by 66% after initial alert

--- ssh_active.py
+++ ssh_active.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/opt/bb/bin/python
 
 """
 Fabric to check SSH connectivity
@@ -8,9 +8,7 @@ from fabric.api import *
 from fabric.exceptions import NetworkError
 from email.mime.text import MIMEText
 
-
 import os
-import requests
 import smtplib
 import whisper
 
@@ -18,6 +16,7 @@ import whisper
 with open('/etc/hosts') as listing:
     raw = listing.readlines()
 env.hosts = [line.split()[1] for line in raw if 'continuous_build' in line and 'global_zone' not in line]
+
 
 
 # Continue on error.
@@ -32,12 +31,10 @@ env.abort_exception = FabricException
 env.parallel = True    # like '-P' for parallel exec
 env.pool_size = 10     # like '-z' for pool-sizing
 env.timeout = 66       # SSH timeout in seconds
-
 env.disable_known_hosts = True  
 env.warn_only = True
 env.skip_bad_hosts = True
-
-env.password = ""    # This is funny...
+env.password = ""
 
 
 def sendmail(hostname, engineer, alert_text):
@@ -56,12 +53,12 @@ def alert(hostname, alert_text):
 
     with open('email.list') as mailing_list:
         emails = mailing_list.readlines()
+
     email = [x.split()[0] for x in emails if '@' in x]
 
     for email_address in email:
-        print "FOUND " + email_address
-
         sendmail(hostname, email_address, alert_text)
+
 
 @task(alias='ssh_check')
 def uptime():
@@ -79,35 +76,72 @@ def uptime():
     if not os.path.exists(whisper_db_name):
         whisper.create(whisper_db_name, retainer, aggregationMethod='last')
 
+
     try:
         with hide('stdout', 'stderr', 'running'):
             try:
                 connector = run('uptime')
 
-            except FabricException as e:                # Catch Fabric errors, like unexpected password prompts
+                ## If this connection was successful AND we are down to a single '1' result, sound an all-clear.
+
+                (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
+
+                if fail_buffer.count(1) == 1 and fail_buffer[0] == 1:
+                    alert(env.host_string, "All clear on " + str(e))
+
+                whisper.update(whisper_db_name, 0)
+                (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
+
+            except FabricException as e:                    # Catch Fabric errors, like unexpected password prompts
                 whisper.update(whisper_db_name, 1)
 
                 (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
-                if fail_buffer.count(1) > 1:
+
+                if fail_buffer.count(1) > 5:
                     alert(env.host_string, str(e))
 
 
-        ## If this connection was successful AND we are down to a single '1' result, sound an all-clear.
+                    # Triple WhisperDB period to reduce alarm repetition
+                    #
 
-        (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
+                    new_whisper_db_name = whisper_db_dir + 'long_' + env.host_string + '.wsp'
 
-        if fail_buffer.count(1) == 1 and fail_buffer[0] == 1:
-            alert(env.host_string, "All clear on " + str(e))
+                    new_retainer = [(retainer[0][0] * 3, 6)]
 
-        whisper.update(whisper_db_name, 0)
+                    print "Instancing temp whisperDB " + new_whisper_db_name + " with retention policy " + str(new_retainer)
 
-        (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
+                    whisper.create(new_whisper_db_name, new_retainer, aggregationMethod='last')
+
+                    whisper.update(new_whisper_db_name, 1)
+
+                    print "Swapping " + new_whisper_db_name + " over " + whisper_db_name
+
+                    os.rename(new_whisper_db_name, whisper_db_name)
 
     except Exception as e:
         whisper.update(whisper_db_name, 1)
 
+        # Count failures
         (times, fail_buffer) = whisper.fetch(whisper_db_name, 315550800)
 
-        if fail_buffer.count(1) > 1:
+        if fail_buffer.count(1) > 5:
             alert(env.host_string, str(e))
+
+
+            # Change WhisperDB period to reduce alarm repetition
+            #
+
+            new_whisper_db_name = whisper_db_dir + 'long_' + env.host_string + '.wsp'
+
+            new_retainer = [(retainer[0][0] * 3, 6)]
+
+            #print "Instancing temp whisperDB " + new_whisper_db_name + " with retention policy " + str(new_retainer)
+
+            whisper.create(new_whisper_db_name, new_retainer, aggregationMethod='last')
+
+            whisper.update(new_whisper_db_name, 1)
+
+            #print "Swapping " + new_whisper_db_name + " over " + whisper_db_name
+
+            os.rename(new_whisper_db_name, whisper_db_name)
 
